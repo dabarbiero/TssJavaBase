36740af3663bfc215ecfbe7584a5868ef2c80049
tss <tss@debian-tss24>
2016-11-14 16:24:49 +0100

    Main

---
+++ src/lezione01/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione01;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tss
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Primo software started......");
+        String frase = JOptionPane.showInputDialog("Inserisci la frase da stampare ....");
+        System.out.println(frase);
+    }
+    
+}
