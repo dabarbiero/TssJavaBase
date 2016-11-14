55893eb8a0f5f1624a65005806c7dcbe0ce3ef50
tss <tss@debian-tss24>
2016-11-14 16:22:29 +0100

    fattoriale

---
+++ src/lezione02/esercizi/Fattoriale.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione02.esercizi;
+
+/**
+ * Legge un numero in input e stampa il suo fattoriale
+ * (fattoriale = indica il prodotto di tutti i numeri naturali (escluso lo 0) minori o uguali a tale numero
+ * 
+ * @author tss
+ */
+public class Fattoriale {
+    
+}
